5779ae66eda48ea2443ae5f76e4b5d506f54103b
Add-Interactive <contact@add-interactive.com>
2026-08-06 21:36:22 -0400

    \ Remove hanging chain motors component\

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import BackstagePassGenerator from './components/BackstagePassGenerator';
 import NashvilleGallery from './components/NashvilleGallery';
 import EmergencySosModal from './components/EmergencySosModal';
 import PeekABooPirate from './components/PeekABooPirate';
-import HangingChainMotors from './components/HangingChainMotors';
 import Footer from './components/Footer';
 
 export default function App() {
@@ -25,9 +24,6 @@ export default function App() {
 
   return (
     <div className="min-h-screen bg-[#0a0a0d] text-slate-100 font-body antialiased selection:bg-amber-500 selection:text-black relative overflow-x-hidden">
-      {/* Interactive Hanging Stage Chain Motors (Top Left & Top Right) */}
-      <HangingChainMotors />
-
       {/* Interactive Pop-Out Peek-A-Boo Rock Pirate (Left Side Screen/Mobile) */}
       <PeekABooPirate />
 
